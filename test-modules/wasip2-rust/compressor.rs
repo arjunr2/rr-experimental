3a005034a1e4da698cabf3a1668ff4d8ca0118a8
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2026-03-17 16:51:07 -0400

    Optimize compressor benchmark

--- test-modules/wasip2-rust/compressor.rs
+++ test-modules/wasip2-rust/compressor.rs
@@ -1,6 +1,6 @@
-use std::fs::{File};
-use std::io::{BufReader, Write};
-use std::path::{PathBuf};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::path::PathBuf;
 use std::error::Error;
 
 use clap::Parser;
@@ -14,7 +14,7 @@ struct Args {
     #[arg(short = 'i', long = "input", value_name = "DIR")]
     input_dir: PathBuf,
 
-    /// Path to the input directory
+    /// Path to the output file
     #[arg(short = 'o', long = "output", value_name = "FILE")]
     output_file: PathBuf,
 
@@ -23,31 +23,19 @@ struct Args {
     level: i32,
 }
 
-//#[unsafe(no_mangle)]
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let mut output_vec = Vec::new();
+    let mut f = BufWriter::new(File::create(args.output_file)?);
+    let mut encoder = zstd::Encoder::new(&mut f, args.level)?;
     for entry in WalkDir::new(&args.input_dir)
         .into_iter()
         .filter_map(Result::ok)
         .filter(|e| e.file_type().is_file())
     {
-        let path = entry.path().to_path_buf();
-        //println!("Compressing: {}", path.display());
-
-        let input_file = File::open(&path)?;
-        let reader = BufReader::new(input_file);
-
-        // Output compressed data into Vec<u8>
-        let mut encoder = zstd::Encoder::new(&mut output_vec, args.level)?;
-        std::io::copy(&mut BufReader::new(reader), &mut encoder)?;
-        encoder.finish()?;
+        let input_file = File::open(entry.path())?;
+        std::io::copy(&mut BufReader::new(input_file), &mut encoder)?;
     }
-
-    let size = output_vec.len();
-    println!("Compressed size: {}", size);
-    let mut f = File::create(args.output_file)?;
-    f.write_all(&output_vec)?;
+    encoder.finish()?;
     Ok(())
 }
